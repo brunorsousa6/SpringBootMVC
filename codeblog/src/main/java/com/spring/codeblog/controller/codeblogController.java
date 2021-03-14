343616cb96853b778b3e7286935c378543bb1ff0
Bruno <brunorsousa06@gmail.com>
2021-03-13 21:42:04 -0300

    criacao de views post e postdetails

--- codeblog/src/main/java/com/spring/codeblog/controller/codeblogController.java
+++ codeblog/src/main/java/com/spring/codeblog/controller/codeblogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,6 +27,16 @@ public class codeblogController {
 		
 		return mv;
 	}
+	
+	@RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+	public ModelAndView getPostDetails(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("postsDetails");
+		Post post = codeblogService.findById(id);
+		mv.addObject("post", post);
+		
+		return mv;
+	}
+	
 	
 
 }
